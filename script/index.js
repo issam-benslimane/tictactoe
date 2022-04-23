e8a502af583fc3ea4180d09616ee4fbaeb2a997b
issam <issambenslimane9999@hotmail.com>
2022-04-23 16:08:03 +0000

    Add Player Function

--- script/index.js
+++ script/index.js
@@ -3,10 +3,14 @@ const GRID_SIZE = 9;
 
 const Gameboard = (function () {
   let board = Array.from({ length: GRID_SIZE }, () => null);
+
   const resetBoard = () => board.fill(null);
-  const addMark = (mark, idx) => (board[idx] = mark);
+
+  const addMark = (mark, pos) => (board[pos] = mark);
+
   const getEmptyCells = () =>
     board.reduce((cum, cell, i) => (cell == null && cum.push(i), cum), []);
+
   const getMarkPositions = (mark) => {
     const pos = [];
     for (let i = 0; i < board.length; i++) {
@@ -18,47 +22,48 @@ const Gameboard = (function () {
   return { getMarkPositions, addMark, resetBoard, getEmptyCells };
 })();
 
+const Player = function ({ name, mark, isAI = false }) {
+  const details = { name, mark, isAI };
+  const score = {
+    win: 0,
+    lose: 0,
+    tie: 0,
+  };
+
+  const setScore = (type) => score[type]++;
+  const setPlayer = (prop, value) => (details[prop] = value);
+
+  return { details, setScore, setPlayer, score };
+};
+
 const Players = (function () {
-  const players = [];
+  const players = {
+    "one player": [
+      Player({ name: "player", mark: "x" }),
+      Player({ name: "computer", mark: "o", isAI: true }),
+    ],
+    "two players": [
+      Player({ name: "player1", mark: "x" }),
+      Player({ name: "player2", mark: "o" }),
+    ],
+  };
   let turn = 0;
 
-  const setPlayers = (...args) => {
-    players.splice(0, 2);
-    for (let [name, mark] of args) {
-      players.push({ name, mark });
-    }
-  };
-  const getPlayers = () => players;
-  const getCurrentPlayer = () => players[turn % 2];
-  const changePlayerName = (name) => {};
+  const getPlayers = (mode) => players[mode];
+  const getCurrentPlayer = (mode) => players[mode][turn % 2];
   const switchTurn = () => turn++;
   const startOver = () => (turn = 0);
-  const handleSwitch = () => {
-    let play = true;
-    const canPlay = () => play;
-    const setCanPlay = (p) => (play = p);
-    return { canPlay, setCanPlay };
-  };
-  const handleAI = () => {
-    let AI = true;
-    const isAiActive = () => AI;
-    const changeOpponent = () => (AI = !AI);
-    return { isAiActive, changeOpponent };
-  };
 
   return {
-    ...handleSwitch(),
-    ...handleAI(),
     getCurrentPlayer,
     switchTurn,
-    changePlayerName,
     startOver,
-    setPlayers,
     getPlayers,
   };
 })();
 
 const GameFlow = (function () {
+  let currentMode = "one player";
   const winningCombinations = [
     [0, 1, 2],
     [3, 4, 5],
@@ -69,94 +74,63 @@ const GameFlow = (function () {
     [0, 4, 8],
     [2, 4, 6],
   ];
-  let comb = null;
+  let winningCombination = null;
 
-  const setComb = (value) => (comb = value);
-  const getComb = () => comb;
+  const getCurrentMode = () => currentMode;
+
   const checkWin = (positions) => {
     return winningCombinations.some((comb) => {
       if (comb.every((e) => positions.includes(e))) {
-        setComb(comb);
+        winningCombination = comb;
         return true;
       }
       return false;
     });
   };
 
-  return { checkWin, setComb, getComb };
+  const getComb = () => winningCombination;
+
+  const endGame = () => {
+    Gameboard.resetBoard();
+    Players.startOver();
+  };
+
+  const changeOpponent = () => {
+    endGame();
+    currentMode = currentMode == "one player" ? "two players" : "one player";
+  };
+
+  return { checkWin, getComb, endGame, changeOpponent, getCurrentMode };
 })();
 
-const Control = (function (obj) {
-  const score = [
-    {
-      x: 0,
-      o: 0,
-      tie: 0,
-    },
-    {
-      x: 0,
-      o: 0,
-      tie: 0,
-    },
-  ];
-  const firstPlayer = document.querySelector(".player1");
-  const secondPlayer = document.querySelector(".player2");
-  const tie = document.querySelector(".tie");
-  const toggle = document.querySelector("input");
-  obj.setPlayers(["player", "x"], ["computer", "o"]);
-  render();
-
-  toggle.addEventListener("change", changeOpponent);
-
-  function render() {
-    const [{ name: name1, mark: mark1 }, { name: name2, mark: mark2 }] =
-      obj.getPlayers();
-    const currentScore = score[+obj.isAiActive()];
-
-    firstPlayer.innerHTML = `<span class="name">${name1} (<span class="mark">${mark1}</span>)</span>
-    <span class="score">${currentScore[mark1]}</span>`;
-    secondPlayer.innerHTML = `<span class="name">${name2} (<span class="mark">${mark2}</span>)</span>
-    <span class="score">${currentScore[mark2]}</span>`;
-    tie.innerHTML = `<span class="name">Tie</span>
-    <span class="score">${currentScore.tie}</span>`;
-  }
-  const updateScore = (s) => {
-    score[+obj.isAiActive()][s]++;
-    render();
-  };
-  const switchPlayersClass = () => {
-    firstPlayer.classList.toggle("active");
-    secondPlayer.classList.toggle("active");
-  };
-
-  return { updateScore, render, switchPlayersClass };
-})(Players);
-
 // DOM manipulation
+
 const board = document.querySelector(".board");
+const firstPlayer = document.querySelector(".player1");
+const secondPlayer = document.querySelector(".player2");
+const tie = document.querySelector(".tie");
+const toggleMode = document.querySelector("input");
+let hasAnimationEnded = true;
 renderCells();
+renderScore();
 
 async function displayMark(pos) {
-  if (!Players.canPlay()) return;
-  const { name, mark } = Players.getCurrentPlayer();
-  playTurn(this, mark, pos);
+  if (!hasAnimationEnded) return;
+
+  const currentMode = GameFlow.getCurrentMode();
+  const currentPlayer = Players.getCurrentPlayer(currentMode);
+  const { name, mark, isAI } = currentPlayer.details;
+
+  Gameboard.addMark(mark, pos);
+  this.classList.add(mark);
 
   await animationEnded(this);
-  Players.setCanPlay(true);
-
-  if (hasPlayerWon(mark)) return gameOver(mark);
-
+  if (hasPlayerWon(mark)) return gameOver(currentPlayer);
   if (isBoardFull()) return gameOver();
 
-  switchTurn();
+  Players.switchTurn();
 
-  if (Players.isAiActive() && name.match("player")) playAiTurn(displayMark);
-}
-
-function playTurn(cell, mark, pos) {
-  cell.classList.add(mark);
-  Gameboard.addMark(mark, pos);
-  Players.setCanPlay(false);
+  if (currentMode == "one player" && !isAI) playAiTurn(displayMark);
 }
 
 function hasPlayerWon(mark) {
@@ -168,11 +142,6 @@ function isBoardFull() {
   return Gameboard.getEmptyCells().length < 1;
 }
 
-function switchTurn() {
-  Players.switchTurn();
-  Control.switchPlayersClass();
-}
-
 function playAiTurn(cb) {
   const emptyCells = Gameboard.getEmptyCells();
   const random = Math.floor(Math.random() * emptyCells.length);
@@ -180,31 +149,30 @@ function playAiTurn(cb) {
   cb.call(randomCell, emptyCells[random]);
 }
 
-function changeOpponent() {
-  Players.changeOpponent();
-  if (this.checked) Players.setPlayers(["player1", "x"], ["player2", "o"]);
-  else Players.setPlayers(["player", "x"], ["computer", "o"]);
-  Gameboard.resetBoard();
-  Control.render();
-  renderCells();
-}
-
 function gameOver(winner = null) {
   if (winner) {
     const winnerComb = GameFlow.getComb();
-    Control.updateScore(winner);
-    gameOverAnimation(winner, winnerComb);
+    endGameAnimation(winner.details.mark, winnerComb);
+    winner.setScore("win");
   } else {
-    Control.updateScore("tie");
-    gameOverAnimation();
+    endGameAnimation();
+    for (let p of Players.getPlayers(GameFlow.getCurrentMode())) {
+      p.setScore("tie");
+    }
   }
-  Gameboard.resetBoard();
-  Players.startOver();
+  renderScore();
+  GameFlow.endGame();
   board.addEventListener("click", restartGame, { capture: true });
 }
 
 function restartGame(ev) {
   ev.stopPropagation();
+  renderCells();
+}
+
+function changeOpponent() {
+  GameFlow.changeOpponent();
+  renderScore();
   renderCells();
 }
 
@@ -219,7 +187,18 @@ function renderCells() {
   }
 }
 
-function gameOverAnimation(winner, comb) {
+function renderScore() {
+  const [p1, p2] = Players.getPlayers(GameFlow.getCurrentMode());
+
+  firstPlayer.innerHTML = `<span class="name">${p1.details.name} (<span class="mark">${p1.details.mark}</span>)</span>
+  <span class="score">${p1.score.win}</span>`;
+  secondPlayer.innerHTML = `<span class="name">${p2.details.name} (<span class="mark">${p2.details.mark}</span>)</span>
+  <span class="score">${p2.score.win}</span>`;
+  tie.innerHTML = `<span class="name">Tie</span>
+  <span class="score">${p2.score.tie}</span>`;
+}
+
+function endGameAnimation(winner, comb) {
   document.querySelectorAll(".cell").forEach((cell, i) => {
     if (!winner) {
       cell.classList.add("cell--tie");
@@ -232,11 +211,15 @@ function gameOverAnimation(winner, comb) {
 
 function animationEnded(el) {
   return new Promise((res) => {
+    hasAnimationEnded = false;
     function handleAnimation(e) {
       if (e.pseudoElement === "before") return;
       el.removeEventListener("animationend", handleAnimation);
+      hasAnimationEnded = true;
       res();
     }
     el.addEventListener("animationend", handleAnimation);
   });
 }
+
+toggleMode.addEventListener("change", changeOpponent);
